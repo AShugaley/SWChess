14ae0af64b05c57d6021b4101e36c517a4720317
Alexander Shugaley <a.shugaley@gmail.com>
2017-08-17 10:35:19 +0300

    added main

---
+++ SW_Chess/SW_Chess/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  SW_Chess
+//
+//  Created by Alexander Shugaley on 17/08/2017.
+//  Copyright © 2017 Alexander Shugaley. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
