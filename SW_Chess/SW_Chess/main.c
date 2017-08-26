a446d1641789e0ce3bb117744aeef01f5669e440
Mor Huberman <morhub58@gmail.com>
2017-08-26 11:26:12 +0300

    updating my tests in the main file - mor

--- SW_Chess/SW_Chess/main.c
+++ SW_Chess/SW_Chess/main.c
@@ -1,21 +1,21 @@
-//
-//  main.c
-//  SW_Chess
-//
-//  Created by Alexander Shugaley on 17/08/2017.
-//  Copyright © 2017 Alexander Shugaley. All rights reserved.
-//
-
 #include <stdio.h>
 #include "Chess_gameUtils.h"
+#include "Chess_gameParser.h"
+#include "Chess_gameSettingsParser.h"
 
 
 
 int main(int argc, const char * argv[]) {
+	//check - ALEX
+	/*
     printf("Hello, World!\n");
     chessGame* src = createChessGame(5, TWO_PLAYERS);
     chessConsolePrintBoard(src);
-    
+    */
+
+	//check - MOR
+	printf("hi! working\n");
+
     
     return 0;
 }
