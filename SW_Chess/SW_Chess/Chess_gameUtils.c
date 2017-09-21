872588b9ed701c4292fd56c6177b742d8d5d5be0
Mor Huberman <morhub58@gmail.com>
2017-09-22 00:27:53 +0300

    merge 1.2

--- SW_Chess/SW_Chess/Chess_gameUtils.c
+++ SW_Chess/SW_Chess/Chess_gameUtils.c
@@ -1,12 +1,10 @@
-<<<<<<< HEAD
 
-=======
->>>>>>> 8fc5c4f... progress GUI - drag&drop works! settings works!
 #include <assert.h>
 #include "Chess_gameUtils.h"
 #include <stdlib.h>
 #include <string.h>
 #include "ChessGUI_Utils.h"
+
 
 /* In all functions, we assume that the arguments are legal - e.g. 0<=pos<=7. We check it elsewhere - the comp will do so by himself, and we will check arguments from the user on input or higher on calculation chain.
  */
